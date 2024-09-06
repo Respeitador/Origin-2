b7c8d9e3e53e6648108b18fa947b6faaf189a5be
Guilherme <vanzan@ufrj.br>
2024-09-06 00:28:05 -0300

    Multi plot, slope, cor de bg

--- origin_2.py
+++ origin_2.py
@@ -1,10 +1,19 @@
+# Python3
+
+# Este programa abre uma planilha em que é possivel selecionar valores para plotar um gráfico de scatter
+# em que a coluna 1 é sempre o eixo X e a linha 1 é a legenda das colunas. Juntamente, faz uma análise
+# estatística de linnear fit, da o valor de slope das linhas e com esse valor, calcula o valor de
+# Ácido Hexurônico nas amostras utilizadas para plotar o gráfico.
+
 from tksheet import Sheet
 import tkinter as tk
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Demo(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.geometry(f"{1100}x{350}")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         self.frame = tk.Frame(self)
@@ -16,7 +25,7 @@ class Demo(tk.Tk):
         self.frame.grid(row=0, column=0, sticky="nswe")
         self.sheet.grid(row=0, column=0, sticky="nswe")
 
-        self.sheet.set_options(table_bg="#34353d")
+        self.sheet.set_options(table_bg="#82C294")
 
         # Add a button to trigger plotting
         self.plot_button = tk.Button(self, text="Plotar Selecionados", command=self.plot_selected)
@@ -30,7 +39,6 @@ class Demo(tk.Tk):
 
         # Get data from selected cells
         x_values = []
-        # y_values = []
         legendas = []
         separate_values = {}
 
@@ -42,10 +50,13 @@ class Demo(tk.Tk):
             else:
                 y_valor = self.sheet.get_cell_data(row, col)
                 if str(col) in separate_values.keys():
-                    separate_values[f'{col}'].append(float(y_valor.replace(',', '.'))) 
+                    try:
+                        separate_values[f'{col}'].append(float(y_valor.replace(',', '.')))
+                    except:
+                        # separate_values[f'{col}'].append(y_valor)
+                        None
 
         row_size = len(separate_values['1']) + 1
-        # print(row_size,separate_values['1'])
         for row in range(row_size):
             if row == 0:  # First row as legend
                 legendas.append(self.sheet.get_cell_data(row, 0))
@@ -53,14 +64,18 @@ class Demo(tk.Tk):
                 x_valor = self.sheet.get_cell_data(row, 0)
                 x_values.append(float(x_valor.replace(',', '.')))
             # print(separate_values)
-
+        x_vectorized = np.array(x_values)[:,np.newaxis]
 
         # Plot values using matplotlib
         for legenda, data in zip(legendas,separate_values.values()):
                 # print(data)
             if len(data) > 0:
-                print(x_values,data.sort())
-                plt.scatter(x_values, data, label=legenda)  # Plot x_values with y_values
+                data = sorted(data)
+                slope, _, _, _ = np.linalg.lstsq(x_vectorized, data)
+                print(f'Slope for {legenda} is {slope}')
+                plt.plot(x_vectorized, slope*x_vectorized)
+                plt.scatter(x_values, data, label=legenda)
+                  # Plot x_values with y_values
         # plt.xticks(x_values, rotation=45)  # Rotate X axis labels
         # plt.yticks(y_values)  # Add Y axis labels
         plt.xlabel("Eixo X")
